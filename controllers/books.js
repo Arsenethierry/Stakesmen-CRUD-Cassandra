fcc11665b3f9671f79476c356b1836c4c0942f59
Arsene <komezarsene15@gmail.com>
2022-06-06 10:15:33 +0530

    Delete operation

--- controllers/books.js
+++ controllers/books.js
@@ -14,13 +14,26 @@ export const getBooks = async(req,res)=>{
 }
 
 export const postBook = async(req,res)=>{
+
     var upsertBook = 'INSERT INTO db2key.books(id,title,author,category) VALUES (?,?,?,?)';
+
     try {
-      client.execute(upsertBook,[types.TimeUuid.now(),req.body.title, req.body.author, req.body.category]);
+      const result = await client.execute(upsertBook,[types.TimeUuid.now(),req.body.title, req.body.author, req.body.category]);
+      res.status(201).json(result);
       console.log('Book Added');
     } catch (error) {
-        res.status(404).send({msg: error});
+        res.status(404).send({ message: error });
         console.log(error)
     }
 }
+
+export const deleteBook = async(req,res)=>{
+    var delBook = "DELETE FROM db2key.books WHERE id = ?";
+    try {
+        client.execute(delBook,[req.params.id]);  
+    } catch (error) {
+        res.status(404).json({ message: "Something went wrong" })
+    }
+}
+
 
--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getBooks, postBook } from '../controllers/books.js';
+import { getBooks, postBook, deleteBook } from '../controllers/books.js';
 
 const router = express.Router();
 
 router.get("/", getBooks)
 router.post("/", postBook);
+router.delete("/:id", deleteBook);
 
 export default router;
